386417d7a1dfe80988b3b9c2ebc4e48f8f82ec76
senaru <s3696653@student.rmit.edu.au>
2020-07-11 12:11:45 +1000

    Adding MongoDB to Express

--- src/server.js
+++ src/server.js
@@ -1,20 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: []
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: []
-    }
-}
+// const articlesInfo = ;
 
 const app = express();
 
@@ -34,7 +22,24 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
     articlesInfo[articleName].comments.push({ username, text });
 
     res.status(200).send(articlesInfo[articleName]);
-}
-)
+})
+
+app.get('/api/articles/:name/', async (req, res) => {
+
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('reactblog');
+
+        const articlesInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articlesInfo)
+
+        client.close();
+    } catch (e) {
+        res.status(500).json({ message: "Error Connecting to db", error });
+    }
+
+})
 
 app.listen(8000, () => console.log("Listening on port 8000"));
